40fc862adc70fd947d7f6fb1dd0cc7a70681dbac
Nnenna Eze <52764648+NnennaE@users.noreply.github.com>
2022-07-23 16:53:12 -0700

    Create displayname.sh

---
+++ displayname.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo
+echo "this script displays users in differnt line"
+
+users="Carolyn Chioma Iyabo Laura Maria Totin"
+echo
+for user in ${users}
+do
+  echo "Hello ${user}"
+done
