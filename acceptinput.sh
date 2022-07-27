1c81e3f712e1f7cdabcac702a22d905ce1174430
Nnenna Eze <52764648+NnennaE@users.noreply.github.com>
2022-07-27 15:35:53 -0700

    Create acceptinput.sh

---
+++ acceptinput.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo " This script combines user input and displays the output"
+echo
+echo "Please enter your personal details"
+echo
+read -p "Enter your firsrname: " surname                                                                                                                                                                                                                                                                                                                    
+read -p " Enter your lastname: " lastname
+read -p " what is your occupatio?: " ocupation
+echo
+echo " Your fullname is $firstname $lastname, and you work as $ocupation"
